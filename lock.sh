53ce6a2cfdabdda1f9878a456ad5ed848a78a0b3
BenBean18 <beakerthespeaker@gmail.com>
2023-02-12 18:12:34 -0500

    lock

---
+++ lock.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+export XDG_RUNTIME_DIR=/run/user/1000
+export WAYLAND_DISPLAY=wayland-1
+
+BLANK='#00000000'
+WHITE='#ffffffff'
+IMAGEPATH='/usr/share/backgrounds/canvas_by_roytanck.jpg'
+swaylock --inside-color=$BLANK        \
+--ring-ver-color=$BLANK          \
+--text-ver-color=$BLANK \
+--text-wrong-color=$BLANK \
+--ring-wrong-color=$BLANK          \
+--ring-color=$BLANK \
+--line-color=$WHITE \
+--line-ver-color=$WHITE \
+--inside-ver-color=$BLANK \
+--inside-color=$BLANK \
+--separator-color=$BLANK \
+--key-hl-color=$WHITE \
+-n \
+--image=$IMAGEPATH \
+
+# Put 
+# #!/bin/sh
+# #if [ "${1}" == "pre" ]; then
+# #  # Do the thing you want before suspend here, e.g.:
+#   su bean /home/bean/.config/sway/lock.sh &
+# #fi
+# in /lib/systemd/system-sleep/lock.sh
